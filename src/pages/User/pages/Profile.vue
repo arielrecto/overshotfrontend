e7b201c24ffbd6b0dd2d89a25d67ce453abacf0d
jennefer <jennefermorabor12@gmail.com>
2023-05-09 16:44:08 +0800

    .

--- src/pages/User/pages/Profile.vue
+++ src/pages/User/pages/Profile.vue
@@ -1,7 +1,145 @@
+
 <script setup>
 
 </script>
-
 <template>
-    profile page    
+    <div class="p-4 sm:flex sm:items-center sm:justify-center sm:py-36">
+        <div class="mx-4 mt-8 flex justify-center sm:mx-10 lg:mx-24">
+            <Sidebar>
+                <button data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar"
+                    aria-controls="separator-sidebar" type="button"
+                    class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                    <span class="sr-only">Open sidebar</span>
+                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path clip-rule="evenodd" fill-rule="evenodd"
+                            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
+                        </path>
+                    </svg>
+                </button>
+
+                <aside id="separator-sidebar"
+                    class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+                    aria-label="Sidebar">
+                    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                        <ul class="space-y-2 font-medium">
+                            <li>
+                                <a href="#"
+                                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <i class="ri-user-fill"></i>
+                                    <span class="ml-3">User Info</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <i class="ri-heart-fill"></i>
+                                    <span class="flex-1 ml-3 whitespace-nowrap">Favorites</span>
+                                    <span
+                                        class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 dark:text-gray-300"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <i class="ri-file-list-fill"></i>
+                                    <span class="flex-1 ml-3 whitespace-nowrap">Orderlist</span>
+                                    <span
+                                        class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <i class="ri-settings-5-fill"></i>
+                                    <span class="flex-1 ml-3 whitespace-nowrap">Setting</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <i class="ri-notification-4-fill"></i>
+                                    <span class="flex-1 ml-3 whitespace-nowrap">Notification</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#"
+                                    class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                                    <i class="ri-logout-box-fill"></i>
+                                    <span class="ml-3">Log out</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+                <form class="flex flex-col space-y-5 p-5 sm:w-8/9 sm:bg-gray-100 sm:rounded-lg sm:drop-shadow-lg">
+                    <div class="w-full my-5 sm:flex sm:justify-center">
+                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-first-name">
+                                First Name
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="grid-first-name" type="text" placeholder="Jane">
+                        </div>
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-last-name">
+                                Last Name
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-last-name" type="text" placeholder="Doe">
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap-mx-2 mb-2">
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-contact">
+                                Contact
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-contact" type="text" placeholder="09091234567">
+                        </div>
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-email">
+                                Email
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-email" type="text" placeholder="exammple@gmail.com">
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap-mx-3 mb-2">
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-location">
+                                Location
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-location" type="text" placeholder="location">
+                        </div>
+
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-zip-code">
+                                Zip Code
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-zip-code" type="text" placeholder="1704">
+                        </div>
+                    </div>
+                    <div class="flex justify-center">
+                        <button class="bg-orange-300 p-2 rounded-lg hover:focus hover:font-bold">Submit</button>
+                    </div>
+                </form>
+            </Sidebar>
+        </div>
+    </div>
 </template>
