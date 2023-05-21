95a248e0aca14373020fb3b967beb7305623d93f
jennefer <jennefermorabor12@gmail.com>
2023-05-21 15:26:00 +0800

    edit profile

--- src/pages/User/pages/Profile.vue
+++ src/pages/User/pages/Profile.vue
@@ -73,73 +73,56 @@
                         </ul>
                     </div>
                 </aside>
-                <form class="flex flex-col space-y-5 p-5 sm:w-8/9 sm:bg-gray-100 sm:rounded-lg sm:drop-shadow-lg">
+                <form class="flex flex-col space-y-5 p-5  sm:bg-gray-100 sm:rounded-lg sm:drop-shadow-lg">
                     <div class="w-full my-5 sm:flex sm:justify-center">
-                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                for="grid-first-name">
-                                First Name
-                            </label>
-                            <input
-                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                id="grid-first-name" type="text" placeholder="Jane">
-                        </div>
-                        <div class="w-full md:w-1/2 px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                for="grid-last-name">
-                                Last Name
-                            </label>
-                            <input
-                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-last-name" type="text" placeholder="Doe">
+                        <label for="first-name" class="block text-sm font-medium text-gray-700">First Name</label>
+                        <div class="mt-1">
+                            <input type="text" name="first-name" id="first-name" autocomplete="family-name"
+                                class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-300 focus:border-orange-300 sm:text-sm">
+                        </div>       
+                    </div>
+                    <div>
+                        <label for="last-name" class="block text-sm font-medium text-gray-700">Last Name</label>
+                        <div class="mt-1">
+                            <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                                class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-300 focus:border-orange-300 sm:text-sm">
                         </div>
                     </div>
-                    <div class="flex flex-wrap-mx-2 mb-2">
-                        <div class="w-full md:w-1/2 px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                for="grid-contact">
-                                Contact
-                            </label>
-                            <input
-                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-contact" type="text" placeholder="09091234567">
-                        </div>
-                        <div class="w-full md:w-1/2 px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                for="grid-email">
-                                Email
-                            </label>
-                            <input
-                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-email" type="text" placeholder="exammple@gmail.com">
+                    <div>
+                        <label for="contact" class="block text-sm font-medium text-gray-700">Contact</label>
+                        <div class="mt-1">
+                            <input type="tel" name="contact" id="contact" autocomplete="tel"
+                                class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-300 focus:border-orange-300 sm:text-sm">
                         </div>
                     </div>
-                    <div class="flex flex-wrap-mx-3 mb-2">
-                        <div class="w-full md:w-1/2 px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                for="grid-location">
-                                Location
-                            </label>
-                            <input
-                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-location" type="text" placeholder="location">
+                    <div>
+                        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                        <div class="mt-1">
+                            <input type="email" name="email" id="email" autocomplete="email"
+                                class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-300 focus:border-orange-300 sm:text-sm">
                         </div>
-
-                        <div class="w-full md:w-1/2 px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                for="grid-zip-code">
-                                Zip Code
-                            </label>
-                            <input
-                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-zip-code" type="text" placeholder="1704">
+                        <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
+                        <div class="mt-1">
+                            <input type="text" name="location" id="location" autocomplete="address-level1"
+                                class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-300 focus:border-orange-300 sm:text-sm">
                         </div>
                     </div>
-                    <div class="flex justify-center">
-                        <button class="bg-orange-300 p-2 rounded-lg hover:focus hover:font-bold">Submit</button>
+                    <div>
+                        <label for="zip-code" class="block text-sm font-medium text-gray-700">Zip Code</label>
+                        <div class="mt-1">
+                            <input type="text" name="zip-code" id="zip-code" autocomplete="postal-code"
+                                class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-300 focus:border-orange-300 sm:text-sm">
+                        </div>
                     </div>
-                </form>
-            </Sidebar>
-        </div>
+                    <div class="col-start-1 md:col-start-2 lg:col-start-3 xl:col-start-4">
+                        <button type="submit"
+                            class="inline-flex justify-center w-full
+                                                            bg-orange-300 px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">
+                            Save changes
+                        </button>
+                    </div>
+                
+            </form>
+        </Sidebar>
     </div>
-</template>
+</div></template>
